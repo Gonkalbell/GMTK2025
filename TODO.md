12ab0a4b80fc9637092ad640f4ebc8bb6b2d1bc4
JM Bell <johnmichaelbell@gmail.com>
2025-07-31 00:57:32 -0400

    adding spline path

--- TODO.md
+++ TODO.md
@@ -1,5 +1,7 @@
 # TODO
 
-- Splines paths
 - Detect when splines form a loop
-- Enemies
+- Enemies / goals to loop around
+- use https://docs.godotengine.org/en/4.4/classes/class_meshdatatool.html#class-meshdatatool to deform coaster-steel-straigt.glm (or coaster-steeltrack.glm)
+- reorganize .glb files in coaster-kit into subdirectories, but find out how to fix up their texture references after moving them
+
--- godot/scenes/player.gd
+++ godot/scenes/player.gd
@@ -2,8 +2,17 @@ extends Node3D
 
 @export var speed: float = 0.25
 @export var turns_per_sec:float = 0.5
+@onready var path: Path3D = %Path3D
 
 func _process(delta: float) -> void:
 	var yaw = Input.get_axis("move_right", "move_left")
 	rotate_y(TAU * yaw * delta * turns_per_sec)
 	translate_object_local(Vector3.FORWARD * speed)
+	var curve = path.curve
+	if curve.point_count > 0:
+		curve.set_point_position(curve.point_count - 1, global_position)
+
+func _on_timer_timeout() -> void:
+	var curve = path.curve
+	path.curve.add_point(global_position)
+	
