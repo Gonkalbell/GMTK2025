fa3188da9fd3921156e0ea4b468bd65bcad33b83
JM Bell <johnmichaelbell@gmail.com>
2025-08-01 00:47:54 -0400

    added spline path back in

--- godot/scenes/coaster.gd
+++ godot/scenes/coaster.gd
@@ -5,6 +5,10 @@ extends Node3D
 @export var origin: Vector3 = Vector3.ZERO
 @export var radius = 10
 
+func _ready() -> void:
+	%Path3D.global_transform = Transform3D.IDENTITY
+	%Path3D.curve.clear_points()
+
 func _process(delta: float) -> void:
 	var yaw_input = Input.get_axis("move_left", "move_right")
 
@@ -25,22 +29,21 @@ func _process(delta: float) -> void:
 	
 	%CameraOrigin.global_basis = Basis(camera_right_dir, camera_up_dir, -camera_forward_dir)
 
-	#var curve = path.curve
-	#if curve.point_count > 0:
-		#curve.set_point_position(curve.point_count - 1, tail_start.global_position)
+	var curve: Curve3D = %Path3D.curve
+	if curve.point_count > 0:
+		curve.set_point_position(curve.point_count - 1, %TailStart.global_position)
 
 func _on_timer_timeout() -> void:
-	return
-	#var curve = path.curve
-	## Detect if our path made a loop
-	#if curve.point_count > 1:
-		#var points = curve.tessellate_even_length(5, 2)
-		#var old_points = points.slice(0, -2)
-		#var intersection = Util.segment_curve_intersect3d(points[-1], points[-2], old_points)
-		#if intersection != null:
-			#var intersection_index = intersection["index"]
-			#var loop_points = points.slice(intersection_index)
-			#curve.set_point_count(intersection_index)
-			#curve.add_point(intersection["point"])
-			#DebugDraw3D.draw_line_path(loop_points, Color.MAGENTA, 1)
-	#path.curve.add_point(tail_start.global_position)
+	var curve: Curve3D = %Path3D.curve
+	# Detect if our path made a loop
+	if curve.point_count > 1:
+		var points = curve.tessellate_even_length(5, 2)
+		var old_points = points.slice(0, -2)
+		var intersection = Util.segment_curve_intersect3d(points[-1], points[-2], old_points)
+		if intersection != null:
+			var intersection_index = intersection["index"]
+			var loop_points = points.slice(intersection_index)
+			curve.set_point_count(intersection_index)
+			curve.add_point(intersection["point"])
+			DebugDraw3D.draw_line_path(loop_points, Color.MAGENTA, 1)
+	curve.add_point(%TailStart.global_position)
