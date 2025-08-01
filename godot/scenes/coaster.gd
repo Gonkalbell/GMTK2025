f57c9ead353b8ea3ef8648a583c599de217220f0
JM Bell <johnmichaelbell@gmail.com>
2025-07-31 21:21:20 -0400

    made player move around a sphere

---
+++ godot/scenes/coaster.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+@export var move_speed: float = 0.1
+@export var turn_speed:float = 0.1
+@export var origin: Vector3 = Vector3.ZERO
+@export var radius = 10
+
+func _process(delta: float) -> void:
+	var yaw = Input.get_axis("move_left", "move_right")
+	
+	rotate_object_local(Vector3.DOWN, TAU * yaw * delta * turn_speed)
+	translate_object_local(Vector3.FORWARD * move_speed)
+
+	var up_dir = (global_position - origin).normalized()
+	var forward_dir = -global_basis.z
+	var right_dir = up_dir.cross(forward_dir).normalized()
+	forward_dir = right_dir.cross(up_dir).normalized()
+	
+	global_position = origin + up_dir * radius
+	global_basis = Basis(right_dir, up_dir, -forward_dir)
+	
+	%CameraOrigin.global_position = %CameraTarget.global_position
+
+	#var curve = path.curve
+	#if curve.point_count > 0:
+		#curve.set_point_position(curve.point_count - 1, tail_start.global_position)
+
+func _on_timer_timeout() -> void:
+	return
+	#var curve = path.curve
+	## Detect if our path made a loop
+	#if curve.point_count > 1:
+		#var points = curve.tessellate_even_length(5, 2)
+		#var old_points = points.slice(0, -2)
+		#var intersection = Util.segment_curve_intersect3d(points[-1], points[-2], old_points)
+		#if intersection != null:
+			#var intersection_index = intersection["index"]
+			#var loop_points = points.slice(intersection_index)
+			#curve.set_point_count(intersection_index)
+			#curve.add_point(intersection["point"])
+			#DebugDraw3D.draw_line_path(loop_points, Color.MAGENTA, 1)
+	#path.curve.add_point(tail_start.global_position)
